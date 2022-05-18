ea62331e2024c20ac91be5fe971411df8d241548
Diogo Lacerda <dyogot30@gmail.com>
2022-05-18 11:29:34 -0300

    Add Patch

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -5,14 +5,19 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/restaurante") //
@@ -30,14 +35,18 @@ public class RestauranteController {
     public List<Restaurante> Listar() {
         return restauranteRepository.Listar();
     }
+
     @GetMapping("/{restauranteId}")
     public ResponseEntity<Restaurante> busca(@PathVariable Long restauranteId) {
-       Restaurante restaurante = restauranteRepository.buscar(restauranteId);
-        if (restaurante != null) {
-            return ResponseEntity.ok(restaurante);
-        }
+//        Restaurante restaurante = restauranteRepository.buscar(restauranteId);
+//        if (restaurante != null) {
+//            return ResponseEntity.ok(restaurante);
+//        }
 
-        return ResponseEntity.notFound().build();
+//        return ResponseEntity.notFound().build();
+        return Optional.ofNullable(restauranteRepository.buscar(restauranteId))
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
@@ -48,7 +57,7 @@ public class RestauranteController {
 
     @PutMapping("/{restauranteId}")
     public ResponseEntity<Restaurante> atualizar(@PathVariable Long restauranteId,
-                                             @RequestBody Restaurante restaurante) {
+                                                 @RequestBody Restaurante restaurante) {
         Restaurante restauranteBd = restauranteRepository.buscar(restauranteId);
 
         if (restauranteBd != null) {
@@ -74,5 +83,32 @@ public class RestauranteController {
         } catch (DataIntegrityViolationException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+                                              @RequestBody Map<String, Object> campos) {
+        Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+        if (restauranteAtual == null) {
+            return ResponseEntity.notFound().build();
+        }
+        merge(campos, restauranteAtual);
+        return atualizar(restauranteId, restauranteAtual);
+    }
+
+    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            field.setAccessible(true);
+
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+//			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+        });
     }
 }
